a84783c68bc3357d6afa4eb73284468cf4bba00a
mapsync <caseyy@mapsync.com>
2018-12-16 16:55:35 -0500

    Create map-page.js

---
+++ src/templates/map-page.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+
+export const MapPageTemplate = ({ title, map }) => {
+
+  return (
+    <iframe src={map}></iframe>
+  )
+}
+
+MapPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  map: PropTypes.string.isRequired,
+}
+
+const MapPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <Layout>
+      <MapPageTemplate
+        title={frontmatter.title}
+        map={frontmatter.map}
+      />
+    </Layout>
+  )
+}
+
+MapPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default MapPage
+
+export const mapPageQuery = graphql`
+  query MapPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title,
+        map
+      }
+    }
+  }
+`
