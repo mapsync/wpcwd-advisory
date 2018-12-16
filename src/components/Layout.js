30c57515b119af3352ce8c2cd28228d69011c8db
mapsync <caseyy@mapsync.com>
2018-12-16 16:51:19 -0500

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -23,22 +23,22 @@ const TemplateWrapper = ({ children }) => (
           <html lang="en" />
 	  <meta charset="UTF-8" />
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	  <meta name="author" content="MapSync" />
           <title>{data.site.siteMetadata.title}</title>
           <meta name="description" content={data.site.siteMetadata.description} />
-          <meta name="mobile-web-app-capable" content="yes">
-	  <meta name="apple-mobile-web-app-capable" content="yes">
-	  <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
-	  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-	  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-	  <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#444444">
-	  <link rel="manifest" href="/img/site.webmanifest">
-	  <meta name="apple-mobile-web-app-title" content="{data.site.siteMetadata.title}">
-	  <meta name="application-name" content="{data.site.siteMetadata.title}">
-	  <meta name="msapplication-TileColor" content="#ffffff">
-	  <meta name="theme-color" content="#ffffff">
-	  <link rel="shortcut icon" href="/img/favicon.ico">
+          <meta name="mobile-web-app-capable" content="yes" />
+	  <meta name="apple-mobile-web-app-capable" content="yes" />
+	  <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+	  <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+	  <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+	  <link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#444444" />
+	  <link rel="manifest" href="/img/site.webmanifest" />
+	  <meta name="apple-mobile-web-app-title" content="{data.site.siteMetadata.title}" />
+	  <meta name="application-name" content="{data.site.siteMetadata.title}" />
+	  <meta name="msapplication-TileColor" content="#ffffff" />
+	  <meta name="theme-color" content="#ffffff" />
+	  <link rel="shortcut icon" href="/img/favicon.ico" />
 	  <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
