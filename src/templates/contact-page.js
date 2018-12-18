723d26f4ba2dd4f68804b73007fe0783eb9fccb8
Gitea <gitea@fake.local>
2018-12-18 18:15:35 -0500

    contact form fixed

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -3,60 +3,14 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import { navigate } from "gatsby-link";
 
 export const ContactPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
 
   
   return (
-    <section className="section">
-      <div className="container">
-        <div class="columns">
-          <div class="column">
-            <PageContent className="content" content={content} />
-          </div>
-          <div class="column">
-            <form
-              name="contact"
-              method="post"
-              action="/contact/thanks/"
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-            >
-              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-              <input type="hidden" name="form-name" value="contact" />
-              <div hidden>
-                <label>
-                  Don’t fill this out:{" "}
-                  <input name="bot-field" />
-                </label>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"name"} >Your name</label>
-                <div className="control">
-                  <input className="input" type={"text"} name={"name"} id={"name"} required={true} />
-                </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"email"}>Email</label>
-                  <div className="control">
-                    <input className="input" type={"email"} name={"email"} id={"email"} required={true} />
-                  </div>
-              </div>
-              <div className="field">
-                <label className="label" htmlFor={"message"}>Message</label>
-                <div className="control">
-                  <textarea className="textarea" name={"message"} id={"message"} required={true} />
-                </div>
-              </div>
-              <div className="field">
-                <button className="button is-primary" type="submit">Send</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
+    <PageContent className="content" content={content} />
   )
 }
 
@@ -69,13 +23,91 @@ ContactPageTemplate.propTypes = {
 const ContactPage = ({ data }) => {
   const { markdownRemark: post } = data
 
+  const encode = function (data) {
+    return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        "name": form.elements["name"].value,
+        "email": form.elements["email"].value,
+        "message": form.elements["message"].value
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
   return (
     <Layout>
-      <ContactPageTemplate
-        contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-      />
+        <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+            <div className="panel">
+                <p className="panel-heading">
+                  Contact Us
+                </p>
+                <div className="panel-block">
+                  <ContactPageTemplate
+                    contentComponent={HTMLContent}
+                    title={post.frontmatter.title}
+                    content={post.html}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              <form
+                name="contact"
+                method="post"
+                action="/contact/success/"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={handleSubmit}
+              >
+                {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                <input type="hidden" name="form-name" value="contact" />
+                <div hidden>
+                  <label>
+                    Don’t fill this out:{" "}
+                    <input name="bot-field" />
+                  </label>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"name"} >Name</label>
+                  <div className="control">
+                    <input className="input" type={"text"} name={"name"} id={"name"} required={true} />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"email"}>Email</label>
+                    <div className="control">
+                      <input className="input" type={"email"} name={"email"} id={"email"} required={true} />
+                    </div>
+                </div>
+                <div className="field">
+                  <label className="label" htmlFor={"message"}>Message</label>
+                  <div className="control">
+                    <textarea className="textarea" name={"message"} id={"message"} required={true} />
+                  </div>
+                </div>
+                <div className="field">
+                  <button className="button is-primary" type="submit">Send</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
