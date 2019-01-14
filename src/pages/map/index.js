d162a52356c9256ad1041b3847cd4482b6644b71
mapsync <caseyy@mapsync.com>
2019-01-14 16:33:19 -0500

    Update index.js

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -5,13 +5,12 @@ import Layout from '../../components/Layout'
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
   }
 
   render() {
     return (
       <Layout>
-        <iframe title="map" src="https://geosyncgo.com/public/wmu-advisory-app"></iframe>
+        <iframe title="map" src={"https://geosyncgo.com/public/wmu-advisory-app" + this.props.location.search}></iframe>
       </Layout>
     );
   }
