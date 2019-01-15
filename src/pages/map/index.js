3b9dddb107083efb88ea91797331a92d5165118e
mapsync <caseyy@mapsync.com>
2019-01-15 08:19:55 -0500

    Update index.js

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -5,6 +5,7 @@ import Layout from '../../components/Layout'
 export default class Index extends React.Component {
   constructor(props) {
     super(props);
+    console.log(props.location.search);
     this.state = { url: "https://geosyncgo.com/public/wmu-advisory-app" + props.location.search };
   }
 
