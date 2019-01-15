c57b133a747cec73e7c9fa335bd4c45c177a4b3e
mapsync <caseyy@mapsync.com>
2019-01-15 08:48:25 -0500

    Update index.js

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -10,7 +10,7 @@ export default class Index extends React.Component {
   render() {
     return (
       <Layout>
-        <iframe title="map" src="https://geosyncgo.com/public/wmu-advisory-app?lat=37.984826239321926&lon=-84.1120543121688&zoom=15&layer=3e9c8cd0-0f78-11e9-9f51-a900fee08ee2&id=f1752b20-ec06-4e73-83b3-53b1dd049fac"></iframe>
+        <iframe title="map" src=`https://geosyncgo.com/public/wmu-advisory-app${this.props.location.search}`></iframe>
       </Layout>
     );
   }
